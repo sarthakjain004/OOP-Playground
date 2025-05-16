e6d33c1d8d4ae4429de54db0e490c8dc46761e5a
GurglingHammer <jainsarthakindia@gmail.com>
2025-05-16 18:22:39 +0530

    Remove unused reference code and comments from Practice_AccessModifiers and Practice_Inheritance files

--- Core_Principles/Practice_AccessModifiers.java
+++ Core_Principles/Practice_AccessModifiers.java
@@ -34,56 +34,6 @@ class Employee{
     }
 }
 
-
-//Please Do not change anything below, It is only for your reference.
-/*
-
-This is the driver code that will execute and demonstrate the functionality of your `Employee` class.
-
-It creates a object of class `Employee`, the parameterised constructor sets the values of nam, employeeId, salary attributes.
-It calls the getSalary method to display the salary set by parameterised constructor.
-It then calls the setSalary mehod to set the newSalary.
-Then simple displayEmployeeDetails is called to prin the details of employee in specified format.
-
-
-// Main class to demonstrate the functionality of the Student class
-public class Main {
-
-    public static void main(String[] args) {
-    
-        Scanner sc = new Scanner(System.in);
-            
-        String name;
-        int employeeId;
-        double salary;
-        double newSalary;
-
-        // Create an Employee object
-        Employee employee = new Employee(name, employeeId, salary);
-
-        // Get and print the salary
-        System.out.printf("Salary : %.2f\n", employee.getSalary());
-
-        // Update the salary
-        employee.setSalary(newSalary);
-
-        // Display employee details
-        employee.displayEmployeeDetails();
-
-        sc.close();
-    }
-}
-
-*/
-
-/*
-//Below is the output format
-
-System.out.println("Name : " + name);
-System.out.println("Employee Id : " + employeeId);
-System.out.printf("Salary : %.2f\n" , salary);
-
-*/
 public class Practice_AccessModifiers {
     public static void main(String[] args) {
         Employee employee = new Employee("John Doe", 12345, 50000);
--- Core_Principles/Practice_Inheritance.java
+++ Core_Principles/Practice_Inheritance.java
@@ -33,51 +33,6 @@ class Engineer extends Employee{
     }
 }
 
-
-//Please Do not change anything below, It is only for your reference.
-/*
-
-This is the driver code that will execute and demonstrate the functionality of your base class `Employee`, driver classes 'Manager', 'Engineer'.".
-
-It creates a object of class `Manager`, uses the parametrised constructor to initialize the attributes of Manager and Employee class.
-It then prints a line with text message "Manager Details".
-Then it calls th displayDetails() method of class Manager which prints details of manager.
-
-It creates a object of class `Engineer`, uses the parametrised constructor to initialize the attributes of Engineer and Employee class.
-It then prints a line with text message "Engineer Details".
-Then it calls th displayDetails() method of class Engineer which prints details of engineer.
-
-
-public class Main {
-
-    public static void main(String[] args) {
-    
-        String M_name, E_name, E_specialization;
-        int M_id, E_id, M_teamSize;
-    
-        // Create Manager instance
-        Manager manager = new Manager(M_name, M_id, M_teamSize);
-        System.out.println("Manager Details");
-        manager.displayDetails();
-
-        // Create Engineer instance
-        Engineer engineer = new Engineer(E_name, E_id, E_specialization);
-        System.out.println("\nEngineer Details");
-        engineer.displayDetails();
-    }
-}
-
-*/
-
-/*
-//Below is the output statements
-
-System.out.println("Name : " + name);
-System.out.println("Id : " + id);
-System.out.println("Team Size : " + teamSize);
-System.out.println("Specialization : " + specialization);
-
-*/ 
 class Practice_Inheritance{
     public static void main(String[] args) {
         // Creating a Manager object
